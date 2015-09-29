3962e55af290c85415360f98acf97aa7cdd3d2c6
unknown <rantalut@EN60638.endava.net>
2015-09-29 14:58:31 +0300

    fixed es6 build to bundle modules

--- gulp/build_js.js
+++ gulp/build_js.js
@@ -5,17 +5,35 @@ var gulp = require('gulp'),
   sourcemaps = require('gulp-sourcemaps'),
   babel = require('gulp-babel'),
   //My modules
-  cvars = require('./cvars');
+  cvars = require('./cvars'),
+  //es6-fy required modules
+  browserify = require("browserify"),
+  babelify = require("babelify"),
+  source = require('vinyl-source-stream'),
+  buffer = require('vinyl-buffer');
 
-build_js = function() {
-  return gulp.src(cvars.src_js)
-    .pipe(gutil.env.es6 === true ? babel() : gutil.noop())
-    .pipe(gutil.env.production === true ? sourcemaps.init() : gutil.noop())
-    .pipe(concat('bundle.js'))
-    //only uglify if gulp is ran with '--type production'
-    .pipe(gutil.env.production === true ? uglify() : gutil.noop())
-    .pipe(gutil.env.production === true ?  sourcemaps.write() : gutil.noop())
-    .pipe(gulp.dest(cvars.dest));
-};
+  build_js = function() {
+    if (gutil.env.es6) {
+      return browserify({
+          entries: cvars.main_js,
+          extensions: ['.js'],
+          debug: false
+        })
+        .transform(babelify)
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(gutil.env.production ? uglify() : gutil.noop())
+        .pipe(gulp.dest(cvars.dest));
+    } else
+      return gulp.src(cvars.src_js)
+        .pipe(gutil.env.production ? sourcemaps.init() : gutil.noop())
+        .pipe(gutil.env.es6 ? babel() : gutil.noop())
+        .pipe(concat('bundle.js'))
+        //only uglify if gulp is ran with '--type production'
+        .pipe(gutil.env.production ? uglify() : gutil.noop())
+        .pipe(gutil.env.production ? sourcemaps.write() : gutil.noop())
+        .pipe(gulp.dest(cvars.dest));
+  };
 
 module.exports = build_js;
--- gulp/cvars.js
+++ gulp/cvars.js
@@ -1,5 +1,6 @@
 cvars = {
   all_js: '../src/**/*.js',
+  main_js:'../src/js/app.js',
   all_css: '../src/**/*.css',
   src_js: '../src/js/**/*.js',
   ext_js: '../src/vendor/js/**/*.js',
